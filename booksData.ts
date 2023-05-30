ff64b2286f8693a13cb378319c5d22a78bd97b92
oussamaZAAM <zaam.oussama@gmail.com>
2023-05-30 22:43:08 +0100

    use mock data

---
+++ booksData.ts
@@ -0,0 +1,12 @@
+import { Book } from "./classes/Book"
+
+const booksData: Book[] = [
+    {
+        id: 1,
+        title: "Subarashiki Hibi",
+        author: "KeroQ",
+        image: "https://cdn.myanimelist.net/images/characters/11/290052.jpg"
+    }
+]
+
+export default booksData;
